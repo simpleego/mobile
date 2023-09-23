a5ef3b657a5d5c01a17957e987f422937c62bc2a
Jong Cheon Park <simpleego@gmail.com>
2023-09-23 20:00:26 +0900

    Add files via upload

---
+++ chap09/MultiPage(CC)/app/src/main/java/kr/co/company/multipage/StartActivity.java
@@ -0,0 +1,12 @@
+package kr.co.company.multipage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.start);
+	}
+}
